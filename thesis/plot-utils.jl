840db17097f279f8fe02bf5580ee489587bbda2a
Marten Lienen <marten.lienen@gmail.com>
2015-07-19 01:36:59 +0200

    Create a helper to plot a sampled PDF correctly

---
+++ thesis/plot-utils.jl
@@ -0,0 +1,22 @@
+using PyPlot
+
+# Plot a histogram with the area under the graph normalized to 1 over all
+# samples
+function plot_histogram(samples, range; bins=200, label="Samples", lw=3)
+    # Select samples, that are in range
+    inrange = filter(s -> range[1] <= s <= range[2], samples)
+
+    histogram = hist(inrange, bins)[2]
+
+    # Normalize to 1 over range
+    w = (range[2] - range[1]) / bins
+    histogram = histogram / (w * sum(histogram))
+
+    # Normalize to 1 over all samples, so that it is congruent with the real PDF
+    N = length(samples)
+    n = length(inrange)
+    histogram = histogram * (n / N)
+
+    X = linspace(range[1], range[2], bins)
+    plot(X, histogram, label=label, lw=lw)
+end
